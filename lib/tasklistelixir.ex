9c4bbbbf345f452ef152f67f9999e10b991cc211
luc50514@gmail.com <luc50514@gmail.com>
2022-06-06 07:44:10 -0400

    Added struck

--- lib/tasklistelixir.ex
+++ lib/tasklistelixir.ex
@@ -1,21 +1,22 @@
 defmodule Tasklistelixir do
-  @moduledoc """
-  Documentation for `Tasklistelixir`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tasklistelixir.hello()
-      :world
-
-  """
+  defstruct top: nil, bottom: nil
   def hello do
     :world
   end
   def new(), do: %{}
+  def new(top, bottom) do
+    %Tasklistelixir{top: top, bottom: bottom}
+  end
+  def value(%Tasklistelixir{top: _, bottom: 0}), do: 0
+  def value(%Tasklistelixir{top: top, bottom: bottom}) do
+    top/bottom
+  end
+  def add(%Tasklistelixir{top: firsttop, bottom: firstbottom},%Tasklistelixir{top: secondtop, bottom: secondbottom}) do
+    new(
+      firsttop * secondbottom + secondtop * firstbottom,
+      firstbottom * secondbottom
+      )
+  end
   def addtask(tasklist, date, body) do
     Map.update(
       tasklist,
--- test/tasklistelixir_test.exs
+++ test/tasklistelixir_test.exs
@@ -11,10 +11,9 @@ defmodule TasklistelixirTest do
   end
 
   test "addtask adds a new task to the map" do
-    task1 = UUID.uuid1()
-    task2 = UUID.uuid1()
-    assert Tasklistelixir.addtask(%{}, task1, ~D[2022-06-05], "Ebill Added") == %{task1 => ["Ebill Added"]}
-    assert Tasklistelixir.addtask(%{}, task2, ~D[2022-06-05], "Ebill2 Added") == %{task2 => ["Ebill2 Added"]}
+    task1 = Tasklistelixir.addtask(%{}, ~D[2022-06-05], "Ebill Added")
+    task2 = Tasklistelixir.addtask(task1, ~D[2022-06-06], "Ebill Added2")
+    assert task2 ==  %{~D[2022-06-05] => ["Ebill Added"], ~D[2022-06-06] => ["Ebill Added2"]}
   end
 
   test "gettask a task from the map" do
@@ -22,6 +21,24 @@ defmodule TasklistelixirTest do
    task2 = Tasklistelixir.addtask(task1, ~D[2022-06-06], "Ebill Added2")
     assert Tasklistelixir.gettask(task2, ~D[2022-06-06]) == ["Ebill Added2"]
     assert Tasklistelixir.gettask(task2, ~D[2022-06-05]) == ["Ebill Added"]
+  end
+
+  test "get struck from tasklist" do
+     assert %Tasklistelixir{top: 1, bottom: 2} == %Tasklistelixir{top: 1, bottom: 2}
+   end
+  test "new generates new struck" do
+    assert Tasklistelixir.new(1, 2) == %Tasklistelixir{top: 1, bottom: 2}
+  end
+  test "value get the values top/bottom" do
+    assert Tasklistelixir.value(%Tasklistelixir{top: 1, bottom: 2}) == 0.50
+  end
+
+  test "value get the values 0 if bottom is 0" do
+    assert Tasklistelixir.value(%Tasklistelixir{top: 1, bottom: 0}) == 0
+  end
+
+  test "Adds the two values get the sum of 1/2 to get 1" do
+    assert Tasklistelixir.add(%Tasklistelixir{top: 1, bottom: 2},%Tasklistelixir{top: 1, bottom: 2}) == %Tasklistelixir{bottom: 4, top: 4}
   end
 
 
